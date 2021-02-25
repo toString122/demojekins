ee3a290461512e008c1646f567f5b82a40f49d60
liwj <liwenji-e@outlook.com>
2021-02-25 15:41:08 +0800

    测试

--- src/main/java/com/liwj/demojekins/DemojekinsApplication.java
+++ src/main/java/com/liwj/demojekins/DemojekinsApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemojekinsApplication {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(DemojekinsApplication.class, args);
     }
